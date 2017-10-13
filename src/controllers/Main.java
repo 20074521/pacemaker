43326d996f56ade38b380e3a17381b1670d19f3c
20074521 <20074521@IT118PC18.wit.ie>
2017-10-13 13:58:58 +0100

    first commit

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -20,7 +20,7 @@ public class Main
 	    logger.log("Creating user list");
 
 	    List<User> users = new ArrayList<User>();
-	    users.add(new User("Bart", "Simpson", "bart@simpson.com", "secret"));
+	    users.add(new User("Bart", "Simpson", "  bart@simpson.com", "secret"));
 	    users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
 	    users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
 	    System.out.println(users);
